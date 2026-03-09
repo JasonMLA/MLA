bc526e0a113d7cfdabaa455a8ad0f2c9b834fe6e
JasonMLA <jasonmlaenterprises@gmail.com>
2026-03-09 11:54:00 -0400

    Add ClinicAcquisitionWebsite React component

---
+++ src/components/ClinicAcquisitionWebsite.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ClinicAcquisitionWebsite: React.FC = () => {
+    return (
+        <div>
+            <h1>Clinic Acquisition Process</h1>
+            <p>This is the Clinic Acquisition Website Component.</p>
+        </div>
+    );
+};
+
+export default ClinicAcquisitionWebsite;
