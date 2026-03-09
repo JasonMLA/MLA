ecf29982122a4ea91289134ab6a2a66a57144160
JasonMLA <jasonmlaenterprises@gmail.com>
2026-03-09 11:54:36 -0400

    Create Clinic Acquisition page

---
+++ pages/clinic-acquisition.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ClinicAcquisitionWebsite from '../components/ClinicAcquisitionWebsite';
+
+const ClinicAcquisition = () => {
+    return <ClinicAcquisitionWebsite />;
+};
+
+export default ClinicAcquisition;
