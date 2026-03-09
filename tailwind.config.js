7e05340d45b2247146aa1c2a57427991b9e53b08
JasonMLA <jasonmlaenterprises@gmail.com>
2026-03-09 11:54:46 -0400

    Create tailwind.config.js for dark theme styling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: '#1a202c',
+        text: '#f7fafc',
+        accent: '#2b6cb0',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
